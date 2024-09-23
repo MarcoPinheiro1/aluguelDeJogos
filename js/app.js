7e841a6ceb6231f16862a98c0784afde77258bc2
Marco Pinheiro <marcopinheirodev@gmail.com>
2024-09-23 18:54:47 -0300

    projeto inicial

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let jogoEscolhido = document.getElementById(`game-${id}`);
+    let imagem = jogoEscolhido.querySelector('.dashboard__item__img');
+    let botao = jogoEscolhido.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoEscolhido.querySelector('.dashboard__item__name');
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
